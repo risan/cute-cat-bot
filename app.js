a9607a2414905b40168925a491a18f4813033044
risan <risanbagja@gmail.com>
2017-01-19 22:24:52 +0100

    :tada: Initial commit.

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const config = require('config');
+
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send("Hello I'm a Cute Cat Bot 🐱");
+});
+
+app.get('/webhook', function (req, res) {
+  if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === config.verify_token) {
+    console.log('Validating token.');
+    return res.send(req.query['hub.challenge']);
+  }
+
+  console.error('👎 Failed validation. Make sure the validation tokens match.');
+  return res.status(403).send('👎 403 Forbidden.');
+});
+
+app.listen(config.port, function () {
+  console.log(`✨ Listening on port ${config.port}.`);
+});
