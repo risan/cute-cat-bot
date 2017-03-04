51cfe9e306b7a0589fcd44ae1b2f635f2be9244e
risan <risanbagja@gmail.com>
2017-03-04 08:48:30 +0100

    :sparkles: Extract cat video urls to its own module.

--- app.js
+++ app.js
@@ -3,21 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const path = require('path');
+const catVideoUrls = require('./cat-video-urls');
 
 const app = express();
-
-const catVideoUrls = [
-  'https://scontent.cdninstagram.com/t50.2886-16/16335418_398293333841559_4167017334034137088_n.mp4',
-  'https://scontent.cdninstagram.com/t50.2886-16/16335586_154715841695104_1246887159885987840_n.mp4',
-  'https://scontent.cdninstagram.com/t50.2886-16/16385689_1931030737129214_1571056752923770880_n.mp4',
-  'https://scontent.cdninstagram.com/t50.2886-16/16379238_151553292013764_3282604209677008896_n.mp4',
-  'https://scontent.cdninstagram.com/t50.2886-16/16404945_284985611915774_88914808098783232_n.mp4',
-  'https://scontent.cdninstagram.com/t50.2886-16/16385426_382513982106819_5222024231516110848_n.mp4',
-  'https://scontent.cdninstagram.com/t50.2886-16/16378886_374700289554509_3011694113780137984_n.mp4',
-  'https://scontent.cdninstagram.com/t50.2886-16/16328083_364353793938155_251732435560038400_n.mp4',
-  'https://scontent.cdninstagram.com/t50.2886-16/16335727_1792497591074384_2194982331276591104_n.mp4',
-  'https://scontent.cdninstagram.com/t50.2886-16/16233075_692689510905166_7549878935236902912_n.mp4',
-];
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
