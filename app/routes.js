da5dec5ca9f4fe3bdde6fb3dd304a0d2ef887a28
risan <risanbagja@gmail.com>
2017-12-12 00:40:31 +0100

    :hammer: Refactor webhook and message received handler

--- app/routes.js
+++ app/routes.js
@@ -1,61 +1,40 @@
 const express = require('express');
 const router = express.Router();
-const request = require('request');
 const config = require('./config');
 const HttpStatus = require('./utils/http-status');
+const Messenger = require('./fb-messenger/messenger');
 const WebhookValidator = require('./fb-messenger/webhook-validator');
 const WebhookHandler = require('./fb-messenger/webhook-handler');
 
+const messenger = new Messenger({pageAccessToken: config.facebook.pageAccessToken, apiVersion: config.facebook.apiVersion})
 const webhookValidator = new WebhookValidator(config.facebook.verifyToken);
 const webhookHandler = new WebhookHandler();
 
+webhookHandler.on('message', onReceivedMessage);
+
 router.get('/webhook', (req, res) => {
   try {
+    console.log('Validating webhook...');
     const challange = webhookValidator.validate(req);
     res.status(HttpStatus.OK).send(challange);
   } catch (e) {
-    console.error(e);
+    console.error(e.message);
     res.status(HttpStatus.FORBIDDEN).send(e.message);
   }
 });
 
 router.post('/webhook', (req, res) => {
-  webhookHandler.handle(req, {
-    onReceivedMessage
-  });
-
+  webhookHandler.handle(req);
+  console.log('Webhook is received 👍');
   return res.status(HttpStatus.OK).send('Webhook is received 👍');
 });
 
 function onReceivedMessage(event) {
   const senderId = event.sender.id;
+  console.log(`Received message from ${senderId}`);
 
-  sendMessage({
-    recipient: {
-      id: senderId
-    },
-    message: {
-      text: 'Hello World 🌎'
-    }
-  });
-}
-
-function sendMessage(data) {
-  request({
-    method: 'POST',
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {
-      access_token: config.facebook.pageAccessToken
-    },
-    json: data
-  }, (error, response, body) => {
-    if (!error && response.statusCode === HttpStatus.OK) {
-      return console.log(`👍 Successfully sent a message #${body.message_id} to ${body.recipient_id}.`);
-    }
-
-    console.error('👎 Unable to send message.');
-    console.error(body.error);
-  });
+  messenger.sendText(senderId, event.message.text.toUpperCase());
+  //messenger.sendImage(senderId, 'https://media1.giphy.com/media/5scVaYq4hKA7u/giphy.gif');
 }
 
 module.exports = router;
