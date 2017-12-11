828fddf0d53e949c5cf8b81d73433891c76308f5
risan <risanbagja@gmail.com>
2017-12-12 00:37:25 +0100

    :sparkles: Add request signature verifier

---
+++ app/fb-messenger/signature-verifier.js
@@ -0,0 +1,32 @@
+const crypto = require('crypto');
+
+class SignatureVerifier {
+  constructor(appSecret) {
+    this.appSecret = appSecret;
+    this.verify = this.verify.bind(this);
+  }
+
+  verify(req, res, buf, encoding) {
+    const signature = req.headers['x-hub-signature'];
+
+    if (!signature) {
+      throw new Error('No signature found on the request');
+    }
+
+    let [method, signatureHash] = signature.split('=');
+
+    const expectedHash = this.hash(buf);
+
+    if (signatureHash !== expectedHash) {
+      throw new Error('Invalid request signature.');
+    }
+  }
+
+  hash(data) {
+    return crypto.createHmac('sha1', this.appSecret)
+      .update(data)
+      .digest('hex');
+  }
+}
+
+module.exports = SignatureVerifier;
